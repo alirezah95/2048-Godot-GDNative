da63ff548e85b1ff69b9513f06ba51a875a32e8f
alirezah95 <alirezahassani95@gmail.com>
2021-05-11 19:16:28 +0430

    Adds background tiles initialization to Board class.

--- Scripts/Src/Board.cpp
+++ Scripts/Src/Board.cpp
@@ -1,4 +1,7 @@
 #include "Board.hpp"
+#include <ResourceLoader.hpp>
+#include <Position2D.hpp>
+#include <Panel.hpp>
 
 using namespace godot;
 
@@ -15,7 +18,27 @@ void Board::_init()
 
 void Board::_ready()
 {
-    Godot::print("_ready from Board class.");
+    m_bg_tile_scn = ResourceLoader::get_singleton()->load(
+        "res://Scenes/BgTile.tscn", "PackedScene");
+    if (m_bg_tile_scn.is_null()) {
+        Godot::print("Null bg_tile_resource");
+    } else {
+        // Instancing background tiles.
+        m_tile_start_pos = get_node<Position2D>("TilesStartPos")->get_position();
+        
+        Vector2 tile_current_positino = m_tile_start_pos + Vector2(4, 4);
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                Panel* new_bg = Object::cast_to<Panel>(
+                    m_bg_tile_scn->instance());
+                new_bg->set_position(tile_current_positino);
+                add_child(new_bg);
+                tile_current_positino.x += 128 + 3;
+            }
+            tile_current_positino.x = m_tile_start_pos.x + 3;
+            tile_current_positino.y += 128 + 4;
+        }
+    }
 
     return;
 }
