7cb83331e3037e96509018f7a908f856bf7b7586
alirezah95 <alirezahassani95@gmail.com>
2021-05-11 19:16:28 +0430

    Adds background tiles initialization to Board class, and adds a README.md file

--- Scripts/Src/Board.hpp
+++ Scripts/Src/Board.hpp
@@ -3,15 +3,19 @@
 
 #include <Node2D.hpp>
 #include <Godot.hpp>
+#include <PackedScene.hpp>
 
 namespace godot
 {
+    class PackedScene;
+    class Panel;
     class Board: public Node2D
     {
         GODOT_CLASS(Board, Node2D)
         
         private:
-
+            Ref<PackedScene> m_bg_tile_scn;
+            Vector2 m_tile_start_pos;
         public:
             static void _register_methods();
 
